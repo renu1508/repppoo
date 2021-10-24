7eae785d9a3946ab7a6174a663093294dd5669e2
pradeep1004 <91699074+pradeep1004@users.noreply.github.com>
2021-10-24 23:56:06 +0530

    Create better-noidea.py

---
+++ better-noidea.py
@@ -0,0 +1,9 @@
+#List comprehensions can do this easily, esp when you leverage that True == 1 and False == 0:
+
+n, m = raw_input().split()
+
+sc_ar = raw_input().split()
+
+A = set(raw_input().split())
+B = set(raw_input().split())
+print sum([(i in A) - (i in B) for i in sc_ar])
